3cb63ff518f004ac004083f68660e8854ed53f7a
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2025-07-11 11:01:59 +0530

    Login is ready

--- RealTimeLocationTracker/Account/Login.aspx.cs
+++ RealTimeLocationTracker/Account/Login.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace RealTimeLocationTracker.Account
 {
@@ -12,6 +14,41 @@ namespace RealTimeLocationTracker.Account
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Unnamed3_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                string name = username.Text.Trim();
+                string psd = password.Text.Trim();
+
+                using(SqlConnection con = DynamicConnectionStringClass.GetConnection())
+                {
+                    string query = "SELECT * FROM signup WHERE username=@name AND password=@psd";
+                    using(SqlCommand cmd=new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@name",name);
+                        cmd.Parameters.AddWithValue("@psd",psd);
+
+                        cmd.Connection.Open();
+
+                        using(SqlDataReader dtr = cmd.ExecuteReader())
+                        {
+                            if (dtr.Read())
+                            {
+                                Session["username"] = dtr["username"].ToString();
+                                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Success : redirecting you to user page.');", true);
+                                Response.Redirect("~/User/UserHome.aspx");
+                            }
+                            else
+                            {
+                                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Error : username or password is incorrect.');", true);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
